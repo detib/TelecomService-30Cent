dcf3a5026bf431cbc3dea3f21890fe9647f70d8a
Blendi Rrustemi <blendirrustemi9@gmail.com>
2022-07-26 05:19:24 +0200

    create, update customer

--- src/CRM/IndividualCustomer.java
+++ src/CRM/IndividualCustomer.java
@@ -2,12 +2,18 @@ package CRM;
 
 import CRM.Contact.IndividualContact;
 import CRM.Enum.Gender;
+import CRM.Enum.STATE;
+import Database.DatabaseConn;
 import lombok.*;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 @Getter
 @ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class IndividualCustomer extends Customer{
     @Setter
     private String name;
@@ -30,5 +36,72 @@ public class IndividualCustomer extends Customer{
         this.dob = dob;
     }
 
+    @Override
+    public boolean create() {
+        Connection con = null;
+        try {
+            con = DatabaseConn.getInstance().getConnection();
+            return con.createStatement().execute(
+                    String.format("INSERT INTO IndividualCustomer VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');",
+                            getId(), name, lastname, idNumber, gender, dob, contact, getCreatedDate(), getState()));
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean update() {
+        Connection con = null;
+        try {
+            con = DatabaseConn.getInstance().getConnection();
+            return con.createStatement().execute(
+                    String.format("UPDATE IndividualCustomer SET `name`='%s'," +
+                                    "`surname`='%s',`gender`='%s',`dob`='%s'," +
+                                    "`contact`='%s',`state`='%s' where CuId='%s';",
+                            name, lastname, gender, dob, contact, getState(), getId()));
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean delete() {
+        return false;
+    }
+
+    @Override
+    public ResultSet findById() {
+        return null;
+    }
+
+    @Override
+    public ResultSet findAll() {
+        return null;
+    }
+
+//    public static void main(String[] args) {
+//        IndividualCustomer i = new IndividualCustomer("Blendi", "RRustemi", 1170609995L,
+//                Gender.M, LocalDate.of(2001, 01, 13));
+//        if(i.create()) {
+//            System.out.println("Created");
+//        } else {
+//            System.out.println("Not Created");
+//        }
+//    }
+
+    public static void main(String[] args) {
+        new IndividualCustomer("aa", "bb", 123132L, Gender.F, LocalDate.of(2000,10,10));
+        new IndividualCustomer("aa", "bb", 123132L, Gender.F, LocalDate.of(2000,10,10));
+        new IndividualCustomer("aa", "bb", 123132L, Gender.F, LocalDate.of(2000,10,10));
+        IndividualCustomer i = new IndividualCustomer("QEJNGsadasdJT", "MBIEMRIIGRUJES", 1235L, Gender.F,
+                LocalDate.of(2001, 01, 13));
+        i.create();
+        i.setName("UNDRRUE");
+        i.setGender(Gender.M);
+        i.setState(STATE.DEACTIVE);
+        i.update();
+    }
 
 }
--- src/Database/JDBC.java
+++ src/Database/JDBC.java
@@ -12,12 +12,9 @@ import java.sql.SQLException;
 public class JDBC {
     public static void main(String[] args) throws SQLException {
 
-
         Connection con =  DatabaseConn.getInstance().getConnection();
 
         ResultSet resultSet = con.createStatement().executeQuery("Show tables;");
-
-
 
         while (resultSet.next()) {
             System.out.printf("%s\n", resultSet.getString("Tables_in_Telecom_20Cent"));
