b02abb8993f6716927e1a20c768bdbf44a3bbb43
detib <detibaholli@gmail.com>
2022-07-26 17:48:02 +0200

    create customer util method

--- src/Util/Util.java
+++ src/Util/Util.java
@@ -1,5 +1,27 @@
 package Util;
 
+import CRM.Contact;
+import CRM.Customer;
+import CRM.Enum.CustomerType;
+
+import java.util.Scanner;
+
 public class Util {
     public final static String phoneNumberRegex = "3834[4-6]\\d{6}";
+
+    public static Customer createCustomer(Scanner sc) {
+        System.out.print("What type of customer are you? (Individual, Business):");
+        CustomerType customerType = CustomerType.valueOf(sc.nextLine().toUpperCase());
+        if(customerType == CustomerType.BUSINESS) {
+            System.out.print("What is your Customer Name: ");
+            String customerName = sc.nextLine();
+            Contact contact = new Contact(ID.CUSTOMER, customerName);
+            return new Customer(CustomerType.BUSINESS, contact);
+        } else if (customerType == CustomerType.INDIVIDUAL) {
+
+            return new Customer(CustomerType.INDIVIDUAL, contact);
+        }
+        return null;
+    }
+
 }
