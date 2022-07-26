85a41963000e491c3303b40906116c6284ff1819
Blendi Rrustemi <blendirrustemi9@gmail.com>
2022-07-26 19:43:44 +0200

    Contract, Subscription classes

--- src/CRM/Contract.java
+++ src/CRM/Contract.java
@@ -1,4 +1,33 @@
 package CRM;
 
+import CRM.Enum.ContractType;
+import CRM.Enum.STATE;
+import Util.ID;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+@Getter
+@ToString(doNotUseGetters = true)
+@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
 public class Contract {
+    private final String id;
+    private ContractType contractType;
+    private final LocalDate createdDate;
+    @Setter
+    private STATE state;
+    @ToString.Exclude
+    private ArrayList<Subscription> subscription;
+
+    public Contract(ContractType contractType) {
+        this.id = ID.CONTRACT.createId();
+        this.contractType = contractType;
+        this.createdDate = LocalDate.now();
+        this.state = STATE.ACTIVE;
+    }
+
 }
--- src/CRM/Subscription.java
+++ src/CRM/Subscription.java
@@ -1,4 +1,37 @@
 package CRM;
 
+import CRM.Enum.ContractType;
+import CRM.Enum.STATE;
+import CRM.Service.Service;
+import Util.ID;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+@Getter
+@ToString(doNotUseGetters = true)
+@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
 public class Subscription {
+    private final String id;
+    private ContractType contractType;
+    private final LocalDate createdDate;
+
+    @Setter
+    private STATE state;
+    @ToString.Exclude
+    private ArrayList<Service> services;
+    @Setter
+    private boolean mobileApp;
+
+    public Subscription(ContractType contractType, boolean mobileApp) {
+        this.id = ID.SUBSCRIPTION.createId();
+        this.contractType = contractType;
+        this.createdDate = LocalDate.now();
+        this.state = STATE.ACTIVE;
+        this.mobileApp = mobileApp;
+    }
 }
