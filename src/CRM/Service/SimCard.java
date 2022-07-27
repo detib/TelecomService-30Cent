c57b4ba2ec9d792ec78304262a3065c85dd42184
detib <detibaholli@gmail.com>
2022-07-27 05:56:34 +0200

    sim card

--- src/CRM/Service/SimCard.java
+++ src/CRM/Service/SimCard.java
@@ -1,4 +1,22 @@
 package CRM.Service;
 
+import lombok.Getter;
+
 public class SimCard implements ServiceType {
+    @Getter
+    private Integer credits; // cents
+
+    public SimCard(Integer credits) {
+        this.credits = credits;
+    }
+
+    @Override
+    public void spendAmount(Integer amount) {
+        this.credits -= amount;
+    }
+
+    @Override
+    public void addAmount(Integer amount) {
+        this.credits += amount;
+    }
 }
